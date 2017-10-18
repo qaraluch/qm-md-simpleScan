2652430dc9b6bb386ca5e4528c32f86d57b175cc
qaraluch <prezenciq@gmail.com>
2017-10-18 18:36:02 +0200

    Import module from atv-import

--- src/index.js
+++ src/index.js
@@ -1,5 +1,115 @@
-function XXX() {
-  return {};
+import { mdHeaderCreate, mdHeaderGetTxt, ifMdHeader } from "qm-rgx-mdheader";
+import { curry, compose, pipe, tap, trace, map, filter, reduce } from "qm-fp";
+
+export default function simpleScan(fileContent, options) {
+  const defaultOptions = {
+    mainMdTopicLevel: 1
+  };
+  const allOptions = Object.assign({}, defaultOptions, options);
+  const state = Object.assign({}, { fileContent, options: allOptions });
+  return Promise.resolve(state)
+    .then(checkPassedFileContent)
+    .then(clearEmptyLines)
+    .then(getMainTopic)
+    .then(getSecondaryTopics)
+    .then(getNumberOfLines)
+    .then(getTopicsCoordinates)
+    .then(getTopicContentCoordinates)
+    .then(extractsContents);
+}
+function checkPassedFileContent(s) {
+  Array.isArray(s.fileContent) || throwWhenfileContentNotArr();
+  return s;
 }
 
-export default XXX;
+const throwWhenfileContentNotArr = () => {
+  throw new Error(
+    "qm-md-simpleScan() - passed file content to scan not inf form of arr!"
+  );
+};
+
+function clearEmptyLines(s) {
+  const clearedFileContent = s.fileContent.filter(l => l !== "");
+  const ns = Object.assign(s, { fileContent: clearedFileContent });
+  return ns;
+}
+
+function getMainTopic(s) {
+  const rgxMainTopic = mdHeaderCreate(s.options.mainMdTopicLevel);
+  const filterMainTopic = ifMdHeader(rgxMainTopic); // curried already
+  const stripTxtFromTopic = mdHeaderGetTxt(rgxMainTopic); // curried already
+  // const traceAs = trace();
+  const getMainTopic = pipe(filter(filterMainTopic), map(stripTxtFromTopic));
+  const contents = [...s.fileContent]; // clone array
+  const [first1MdTopicTxt] = getMainTopic(contents);
+  const ns = Object.assign(s, { topicMain: first1MdTopicTxt });
+  return ns;
+}
+
+function getSecondaryTopics(s) {
+  const rgxSecondaryTopic = mdHeaderCreate(s.options.mainMdTopicLevel + 1); // level deeper
+  const filterSecondaryTopic = ifMdHeader(rgxSecondaryTopic); // curried already
+  const stripTxtFromTopic = mdHeaderGetTxt(rgxSecondaryTopic); // curried already
+  // const traceAs = trace();
+  const getAllSecondaryTopics = pipe(
+    filter(filterSecondaryTopic),
+    map(stripTxtFromTopic)
+  );
+  const contents = [...s.fileContent]; // clone array
+  const result = getAllSecondaryTopics(contents);
+  const ns = Object.assign(s, { topicSecondary: result });
+  return ns;
+}
+
+function getNumberOfLines(s) {
+  const ns = Object.assign(s, {
+    coordinatesLinesNumber: s.fileContent.length - 1
+  }); // 0 idexed
+  return ns;
+}
+
+function getTopicsCoordinates(s) {
+  const rgxSecondaryTopic = mdHeaderCreate(s.options.mainMdTopicLevel + 1); // level deeper
+  const isIt2topic = ifMdHeader(rgxSecondaryTopic); // curried already
+  const compute2MdCoords = (txt, index) => isIt2topic(txt) && index;
+  const removeFalses = line => line !== false;
+  const getSecoundTopicCoords = pipe(
+    map(compute2MdCoords),
+    filter(removeFalses)
+  );
+  const contents = [...s.fileContent]; // clone array
+  const allSecoundTopicCoords = getSecoundTopicCoords(contents);
+  const ns = Object.assign(s, {
+    coordinatesSecondaryTopics: allSecoundTopicCoords
+  });
+  return ns;
+}
+
+function getTopicContentCoordinates(s) {
+  const topics = s.coordinatesSecondaryTopics;
+  const endFile = s.coordinatesLinesNumber;
+  const ends = [];
+  const computeCoords = (topic, i, arr) => {
+    const computeEnd = () => {
+      const isItLastTopic = i === arr.length - 1;
+      return isItLastTopic ? endFile : arr[i + 1] - 1; // line before next subtopic
+    };
+    ends.push(computeEnd());
+  };
+  topics.map(computeCoords);
+  const ns = Object.assign(s, { coordinatesTopicEnds: ends });
+  return ns;
+}
+
+function extractsContents(s) {
+  const file = [...s.fileContent]; // clone array
+  const starts = s.coordinatesSecondaryTopics;
+  const ends = s.coordinatesTopicEnds;
+  const cutContents = (c, i) => {
+    return file.slice(c, ends[i] + 1);
+  };
+
+  const extract = pipe(map(cutContents));
+  const ns = Object.assign(s, { contents: extract(starts) });
+  return ns;
+}
--- test/index.test.js
+++ test/index.test.js
@@ -1,16 +1,157 @@
 import test from "ava";
-import XXX from "../dist/index.js";
+import sscan from "../dist/index.js";
 
-test("constructor function check", t => {
-  const msg = "should be a function i.e. construction fn.";
-  const actual = typeof XXX === "function";
+/**
+ * Simple scan md file. When you have one main topic and only one deep sub topics.
+ * Extract topic text and contents of defined main topic (e.g. #).
+ * Extract text of topics and contents of one step deeper (i.e. ##).
+ *
+ * Extract first accountered main topics and
+ * its content to first accountered secondary topic.
+ *
+ * Contents of the secoundary topics are computed between ## headers
+ *
+ * Not saving content of main Topic yet.
+ *
+ * When not found return state with empty arrays or undefined values!
+ */
+
+const fileContent = [
+  "",
+  "# topic",
+  "Contents of main topic!",
+  "## subtopic1",
+  "Contents of subtopic topic!",
+  "## subtopic2",
+  "Terefere",
+  "Ququ",
+  "Benc",
+  "",
+  "",
+  "## subtopic3",
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.",
+  "## subtopic4",
+  "## subtopic5",
+  ""
+];
+
+const fileContentCleared = [
+  "# topic",
+  "Contents of main topic!",
+  "## subtopic1",
+  "Contents of subtopic topic!",
+  "## subtopic2",
+  "Terefere",
+  "Ququ",
+  "Benc",
+  "## subtopic3",
+  "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod.",
+  "## subtopic4",
+  "## subtopic5"
+];
+
+test("is function", t => {
+  const msg = "should be a function ";
+  const actual = typeof sscan === "function";
   const expected = true;
   t.is(actual, expected, msg);
 });
 
-test("init", t => {
-  const msg1 = "should construction fn. return an object i.e. state";
-  const actual1 = typeof XXX() === "object";
-  const expected1 = true;
+test("return promise", async t => {
+  const msg = "should return a promise ";
+  const actual = typeof await sscan(fileContent).then === "function";
+  const expected = true;
+  t.is(actual, expected, msg);
+});
+
+test("options and input check", async t => {
+  const msg1 = "should default mainMdTopicLevel be set on 1";
+  const state = await sscan(fileContent);
+  const actual1 = state.options.mainMdTopicLevel;
+  const expected1 = 1; // default level
   t.is(actual1, expected1, msg1);
+  const msg2 = "should be passed an arr of the content of scaning file";
+  const actual2 = state.fileContent.length;
+  const expected2 = 0;
+  t.not(actual2, expected2, msg2);
+});
+
+test("throw error not passed fileContent", async t => {
+  const msg2 = "should throw an error passed not fileContent in form of arr";
+  const error2 = await t.throws(sscan("string"));
+  t.is(
+    error2.message,
+    "qm-md-simpleScan() - passed file content to scan not inf form of arr!",
+    msg2
+  );
+});
+
+test("clear empty lines", async t => {
+  const msg = "should clear empty lines in file content";
+  const file = await sscan(fileContent);
+  const actual = file.fileContent;
+  const expected = fileContentCleared;
+  t.deepEqual(actual, expected, msg);
+});
+
+test("pull main topic", async t => {
+  const msg = "should read the content of file and read content of # topic";
+  const file = await sscan(fileContent);
+  const actual = file.topicMain;
+  const expected = "topic";
+  t.is(actual, expected, msg);
+});
+
+test("pull secondary topics", async t => {
+  const msg = "should read the content of file and read content of ## topic";
+  const file = await sscan(fileContent);
+  const actual = file.topicSecondary;
+  const expected = [
+    "subtopic1",
+    "subtopic2",
+    "subtopic3",
+    "subtopic4",
+    "subtopic5"
+  ];
+  t.deepEqual(actual, expected, msg);
+});
+
+test("read number of lines in a file", async t => {
+  const msg = "should read the content of file and get number of lines";
+  const file = await sscan(fileContent);
+  const actual = file.coordinatesLinesNumber;
+  const expected = 11; // 0 idexed
+  t.deepEqual(actual, expected, msg);
+});
+
+test("calculate secondary topics lines numbers", async t => {
+  const msg = "should read the content of file and get line number";
+  const file = await sscan(fileContent);
+  const actual = file.coordinatesSecondaryTopics;
+  const expected = [2, 4, 8, 10, 11];
+  t.deepEqual(actual, expected, msg);
+});
+
+test("calculate secoundary topics end coordinates", async t => {
+  const msg =
+    "should read the content of file and copute 2 topic end coordinate";
+  const file = await sscan(fileContent);
+  const actual = file.coordinatesTopicEnds;
+  const expected = [3, 7, 9, 10, 11];
+  t.deepEqual(actual, expected, msg);
+});
+
+test("store sutopics content", async t => {
+  const msg =
+    "should read the content of file and extract contents of each subtopic";
+  const file = await sscan(fileContent);
+  const actual = file.contents.map(arr => arr.slice(0, 1)); // only first item from arr
+  const expected = [
+    ["## subtopic1"],
+    ["## subtopic2"],
+    ["## subtopic3"],
+    ["## subtopic4"],
+    ["## subtopic5"]
+  ];
+  t.deepEqual(actual, expected, msg);
 });
